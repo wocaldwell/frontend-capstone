e09933ec12ff2d8fd1fcb479f31a456c88888c93
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-14 11:36:44 -0500

    Added basic html for splash page

--- app/app.js
+++ app/app.js
@@ -2,3 +2,12 @@
 
 // defining app (remember to assign '"app": true' in your Gruntfile.js in grunt.initConfig under 'Globals"')
 let app = angular.module("ArriveDry", ["ngRoute"]);
+
+// displaying different routing available with which partial and controller assigned to them
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "partials/getStarted.html",
+        controller: "GetStartedCtrl"
+    });
+});
