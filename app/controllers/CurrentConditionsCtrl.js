4326c1855e449b4a6ea224d845512146648db1d3
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-15 10:12:22 -0500

    basic conditions partion complete and receiving data from api.

---
+++ app/controllers/CurrentConditionsCtrl.js
@@ -0,0 +1,17 @@
+"use strict";
+
+app.controller("CurrentConditionsCtrl", function($scope, $window, $location, LocationFactory, WeatherFactory, WeatherCredentials) {
+    $scope.apiKey = WeatherCredentials.apiKey;
+    WeatherFactory.getConditions()
+    .then(function(conditions) {
+        console.log('The conditions object is: ', conditions);
+        $scope.locationLat = conditions.display_location.latitude;
+        $scope.locationLon = conditions.display_location.longitude;
+        $scope.icon = conditions.icon_url;
+        $scope.iconText = conditions.icon;
+        $scope.temperature = conditions.temp_f;
+        $scope.feelsLikeTemperature = conditions.feelslike_f;
+        $scope.wind = conditions.wind_string;
+    });
+    // $scope.myCoords = LocationFactory.getMyCoords();
+});
