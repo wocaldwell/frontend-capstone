7f479571738cf172ef8cade337baca485a0b37e2
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-17 10:42:34 -0500

    added links to radat and waether that direct to wu pages.

--- app/controllers/CurrentConditionsCtrl.js
+++ app/controllers/CurrentConditionsCtrl.js
@@ -6,13 +6,19 @@ app.controller("CurrentConditionsCtrl", function($scope, $window, $location, Loc
     WeatherFactory.getConditions()
     .then(function(conditions) {
         console.log('The conditions object is: ', conditions);
-        $scope.locationLat = conditions.display_location.latitude;
-        $scope.locationLon = conditions.display_location.longitude;
+        let longLat = conditions.display_location.latitude,
+            longLon = conditions.display_location.longitude;
+        longLat = parseFloat(longLat);
+        longLon = parseFloat(longLon);
+        $scope.locationLat = longLat.toFixed(2);
+        $scope.locationLon = longLon.toFixed(2);
         $scope.icon = conditions.icon_url;
         $scope.iconText = conditions.icon;
         $scope.temperature = conditions.temp_f;
         $scope.feelsLikeTemperature = conditions.feelslike_f;
         $scope.wind = conditions.wind_string;
+        $scope.locationCity = conditions.display_location.city;
+        $scope.locationState = conditions.display_location.state;
     });
 
     // $scope.myCoords = LocationFactory.getMyCoords();
