296345cca38e4e0ad42e5883ca6c73f0397a9a5b
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-17 14:52:53 -0500

    reconfigured CurrentConditions so that the coords call is the start of the promise chain.

--- app/controllers/CurrentConditionsCtrl.js
+++ app/controllers/CurrentConditionsCtrl.js
@@ -1,23 +1,23 @@
 "use strict";
 
 app.controller("CurrentConditionsCtrl", function($scope, $window, $location, LocationFactory, WeatherFactory, WeatherCredentials, $routeParams) {
+
     $scope.apiKey = WeatherCredentials.apiKey;
 
 
-    LocationFactory.myPosition()
-    .then(LocationFactory.getMyCoords());
+    LocationFactory.getGeolocation()
+    .then(function(returnedCoords) {
+        WeatherFactory.getConditions(returnedCoords)
+        .then(function(conditions) {
+            console.log('The conditions object is: ', conditions);
+            $scope.locationLat = conditions.display_location.latitude;
+            $scope.locationLon = conditions.display_location.longitude;
+            $scope.icon = conditions.icon_url;
+            $scope.iconText = conditions.icon;
+            $scope.temperature = conditions.temp_f;
+            $scope.feelsLikeTemperature = conditions.feelslike_f;
+            $scope.wind = conditions.wind_string;
+        });
+    });
 
-    // WeatherFactory.getConditions()
-    // .then(function(conditions) {
-    //     console.log('The conditions object is: ', conditions);
-    //     $scope.locationLat = conditions.display_location.latitude;
-    //     $scope.locationLon = conditions.display_location.longitude;
-    //     $scope.icon = conditions.icon_url;
-    //     $scope.iconText = conditions.icon;
-    //     $scope.temperature = conditions.temp_f;
-    //     $scope.feelsLikeTemperature = conditions.feelslike_f;
-    //     $scope.wind = conditions.wind_string;
-    // });
-
-    // $scope.myCoords = LocationFactory.getMyCoords();
 });
